25c84253861058bc6948540966f746ec17c32cae
Bruno De Masi <brunodemasi1@gmail.com>
2023-03-02 23:49:27 -0300

    feat(stores): crud

--- backend/src/controllers/store-controller.ts
+++ backend/src/controllers/store-controller.ts
@@ -1,12 +1,11 @@
-import { User } from '@prisma/client';
+import { Store, User } from '@prisma/client';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { prismaClient } from '../database/prisma-client';
+import { HttpException } from '../utils/helpers/http-exception';
 
 class StoreController {
 	public async create(req: FastifyRequest, reply: FastifyReply) {
-		const { name } = req.body as {
-			name: string;
-		};
+		const { name, description, networks } = req.body as Store;
 
 		try {
 			const user = req.user as User;
@@ -16,6 +15,8 @@ class StoreController {
 				data: {
 					owner_id: ownerId,
 					name,
+					description,
+					networks,
 				},
 			});
 
@@ -23,7 +24,56 @@ class StoreController {
 				success: true,
 			});
 		} catch (e) {
-			return e;
+			throw HttpException.badRequest();
+		}
+	}
+
+	public async edit(req: FastifyRequest, reply: FastifyReply) {
+		const { id } = req.params as {
+			id: string;
+		};
+
+		const body = req.body as Partial<Store>;
+
+		try {
+			const updatedStore = await prismaClient.store.update({
+				where: {
+					id,
+				},
+				data: body,
+			});
+
+			reply.send(updatedStore);
+		} catch (e) {
+			throw HttpException.badRequest();
+		}
+	}
+
+	public async list(_: FastifyRequest, reply: FastifyReply) {
+		try {
+			const stores = await prismaClient.store.findMany();
+
+			reply.send(stores);
+		} catch (e) {
+			throw HttpException.badRequest();
+		}
+	}
+
+	public async delete(req: FastifyRequest, reply: FastifyReply) {
+		const { id } = req.params as {
+			id: string;
+		};
+
+		try {
+			await prismaClient.store.delete({
+				where: {
+					id,
+				},
+			});
+
+			reply.send();
+		} catch (e) {
+			throw HttpException.badRequest();
 		}
 	}
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -46,6 +46,28 @@ const user = async (app: FastifyInstance) => {
 
 const store = async (app: FastifyInstance) => {
 	return [
+		app.put(
+			'/edit/:id',
+			{
+				preHandler: app.auth([app.verifyJWT]),
+				schema: {
+					body: {
+						type: 'object',
+						properties: {
+							owner_id: { not: {} },
+							name: { type: 'string' },
+							description: { type: 'string' },
+							networks: {
+								type: 'array',
+								items: { type: 'string' },
+								default: [],
+							},
+						},
+					},
+				},
+			},
+			storeController.edit
+		),
 		app.post(
 			'/create',
 			{
@@ -53,14 +75,34 @@ const store = async (app: FastifyInstance) => {
 				schema: {
 					body: {
 						type: 'object',
-						required: ['name'],
+						required: ['name', 'description', 'networks'],
 						properties: {
 							name: { type: 'string' },
+							description: { type: 'string' },
+							networks: {
+								type: 'array',
+								items: { type: 'string' },
+								default: [],
+							},
 						},
 					},
 				},
 			},
 			storeController.create
+		),
+		app.get(
+			'/list',
+			{
+				preHandler: app.auth([app.verifyJWT]),
+			},
+			storeController.list
+		),
+		app.delete(
+			'/delete/:id',
+			{
+				preHandler: app.auth([app.verifyJWT]),
+			},
+			storeController.delete
 		),
 	];
 };
