d6705ec6907b1518d4d8d8c2bb39529002082688
Bruno De Masi <brunodemasi1@gmail.com>
2023-03-05 17:41:43 -0300

    feat: fastify-cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify, { FastifyInstance } from 'fastify';
 import fastifyAuth from '@fastify/auth';
 import fastifyJwt from '@fastify/jwt';
+import fastifyCors from '@fastify/cors';
+
 import config from './config';
 import { router } from './routes';
 import { verifyJWT } from './middlewares/authorization';
@@ -15,8 +17,13 @@ const mount = async () => {
 
 	app.decorate('verifyJWT', verifyJWT);
 
-	// TODO: cors
-	// TODO: sessioninit
+	await app.register(fastifyCors, {
+		methods: 'HEAD, OPTIONS, PUT, POST, PATCH, GET, DELETE',
+		allowedHeaders: 'content-type, authorization, x-usr-addr',
+		credentials: true,
+		maxAge: 1000 * 60 * 24,
+		origin: '*',
+	});
 
 	await router(app);
 
